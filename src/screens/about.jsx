cee1f894b02171ea2812f7b7e507a3388b68ea2e
mohamed amin <mohamedamin0az@gmail.com>
2024-01-21 14:31:11 +0200

    ui enhancement

--- src/screens/about.jsx
+++ src/screens/about.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Image,
@@ -7,9 +7,21 @@ import {
   Linking,
   TouchableOpacity,
   ScrollView,
+  Dimensions,
 } from 'react-native';
 
 const About = () => {
+  const [phoneOrientation, setOrientation] = useState('PORTRAIT');
+
+  useEffect(() => {
+    Dimensions.addEventListener('change', ({window: {width, height}}) => {
+      if (width < height) {
+        setOrientation('PORTRAIT');
+      } else {
+        setOrientation('LANDSCAPE');
+      }
+    });
+  }, []);
   const handlePress1 = () => {
     Linking.openURL(
       'https://www.tiktok.com/@alnaharaljadid?_t=8j8JRrusbX4&_r=1',
@@ -28,26 +40,44 @@ const About = () => {
   };
   return (
     <ScrollView style={styles.container}>
+      <Text
+        style={{
+          ...styles.title,
+          padding: phoneOrientation == 'PORTRAIT' ? '8%' : '3%',
+        }}>
+        About AlnharAljaded Aljaded
+      </Text>
       <Image source={require('../assets/logo.png')} style={styles.logo} />
-      <Text style={{padding: 7}}>
+      <Text style={{padding: 10, marginTop: 17}}>
         ALNAHARALJADID satellite channel A diverse Gulf Arab channel that began
         broadcasting in February 2022 It presents international foreign
         entertainment content without breaks It includes some Arabic, cultural
         and medical programs Frequency 11555 vertical
       </Text>
       <Text style={{...styles.heading, marginTop: 30}}>find us on:</Text>
-      <TouchableOpacity onPress={handlePress1} style={styles.icon}>
-        <Image source={require('../assets/tiktok.png')} style={styles.icon} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handlePress2} style={styles.icon}>
-        <Image source={require('../assets/instgram.png')} style={styles.icon} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handlePress3} style={styles.icon}>
-        <Image source={require('../assets/youtube.png')} style={styles.icon} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handlePress4} style={styles.icon}>
-        <Image source={require('../assets/facebook.png')} style={styles.icon} />
-      </TouchableOpacity>
+      <View style={styles.social}>
+        <TouchableOpacity onPress={handlePress1} style={styles.icon}>
+          <Image source={require('../assets/tiktok.png')} style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handlePress2} style={styles.icon}>
+          <Image
+            source={require('../assets/instgram.png')}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handlePress3} style={styles.icon}>
+          <Image
+            source={require('../assets/youtube.png')}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handlePress4} style={styles.icon}>
+          <Image
+            source={require('../assets/facebook.png')}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 };
@@ -69,7 +99,16 @@ const styles = StyleSheet.create({
   icon: {
     width: 70,
     height: 70,
-    marginBottom: 10,
+  },
+  social: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'black',
   },
 });
 
--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Dimensions} from 'react-native';
 import Video from 'react-native-video';
 
 const Home = () => {
+  const [orientation, setOrientation] = useState('PORTRAIT');
+
+  useEffect(() => {
+    Dimensions.addEventListener('change', ({window: {width, height}}) => {
+      if (width < height) {
+        setOrientation('PORTRAIT');
+      } else {
+        setOrientation('LANDSCAPE');
+      }
+    });
+  }, []);
+
   return (
     <View
       style={{
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
       }}>
       <Video
-        style={{width: '100%', height: '80%'}}
-        resizeMode="stretch"
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+        resizeMode={orientation == 'PORTRAIT' ? '' : 'stretch'}
         repeat
-        source={{uri: 'https://ffs1.gulfsat.com/ALNAHAR_ALJADID/video.m3u8'}}
+        // source={{uri: 'https://ffs1.gulfsat.com/ALNAHAR_ALJADID/video.m3u8'}}
+        source={{
+          uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+        }}
       />
     </View>
   );
