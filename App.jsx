be70abd2c677f04bf344d3c5c847515b74289064
mohamed amin <mohamedamin0az@gmail.com>
2024-01-23 15:21:41 +0200

    delete commented code

--- App.jsx
+++ App.jsx
@@ -4,8 +4,6 @@ import StackScreens from './src/navigation/stackNavigator';
 import messaging from '@react-native-firebase/messaging';
 import {Alert} from 'react-native';
 const App = () => {
-  const [notificationData, setNotificationData] = useState(null);
-
   useEffect(() => {
     // Subscribe the device to the 'allUsers' topic
     const subscribeToTopic = async () => {
@@ -16,23 +14,13 @@ const App = () => {
         console.error('Error subscribing to the topic:', error);
       }
     };
-
-    // Call the subscription function
     subscribeToTopic();
-
     // Set up a listener for incoming messages
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       const {title, body} = remoteMessage.data;
-
-      // Update the state with notification data
-      setNotificationData({title, body});
-
-      // Display an alert with the message content
       Alert.alert(title, body);
     });
-
     return () => {
-      // Clean up the listener when the component unmounts
       unsubscribe();
     };
   }, []);
