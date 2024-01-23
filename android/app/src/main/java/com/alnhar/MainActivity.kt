7215e50834dfb65fca856fbb0f4500b49a4af40c
mohamed amin <mohamedamin0az@gmail.com>
2024-01-23 21:50:44 +0200

    add maximize button

--- android/app/src/main/java/com/alnhar/MainActivity.kt
+++ android/app/src/main/java/com/alnhar/MainActivity.kt
@@ -4,8 +4,16 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-
+import android.content.Intent
+import android.content.res.Configuration
 class MainActivity : ReactActivity() {
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    val intent = Intent("onConfigurationChanged")
+    intent.putExtra("newConfig", newConfig)
+    sendBroadcast(intent)
+}
+
  override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
   }
--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import {View, Dimensions} from 'react-native';
+import {View, Dimensions, StyleSheet, Pressable} from 'react-native';
 import Video from 'react-native-video';
+import Orientation from 'react-native-orientation-locker';
+import Icon from 'react-native-vector-icons/Feather';
 
 const Home = () => {
   const [orientation, setOrientation] = useState('PORTRAIT');
@@ -14,26 +16,50 @@ const Home = () => {
       }
     });
   }, []);
+  const [isLocked, setIsLocked] = useState(true);
 
+  useEffect(() => {
+    if (isLocked) {
+      Orientation.lockToLandscape();
+    } else {
+      Orientation.lockToPortrait();
+    }
+  }, [isLocked]);
+
+  const toggleOrientation = () => {
+    setIsLocked(!isLocked);
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-      }}>
-      <Video
-        style={{
-          width: '100%',
-          height: '100%',
-        }}
-        resizeMode={orientation == 'PORTRAIT' ? '' : 'stretch'}
-        repeat
-        // source={{uri: 'https://ffs1.gulfsat.com/ALNAHAR_ALJADID/video.m3u8'}}
-        source={{
-          uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-        }}
-      />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Video
+          style={styles.video}
+          resizeMode={orientation == 'PORTRAIT' ? '' : 'stretch'}
+          repeat
+          // source={{uri: 'https://ffs1.gulfsat.com/ALNAHAR_ALJADID/video.m3u8'}}
+          source={{
+            uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+          }}
+        />
+        <Pressable onPress={toggleOrientation} style={styles.icon}>
+          <Icon name="maximize-2" size={35} color="#900" />
+        </Pressable>
+      </View>
+    </>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+  },
+  icon: {
+    position: 'absolute',
+    top: '0%',
+    right: '1%',
+  },
+  video: {
+    width: '100%',
+    height: '100%',
+  },
+});
 export default Home;
